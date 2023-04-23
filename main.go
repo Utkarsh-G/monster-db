bdb3ca9b9f170c2d0e18aba86427538897b075ca
Utkarsh Gaur <charmedtobe@gmail.com>
2023-04-23 18:12:01 -0400

    probably working server. with autogen from buf generate.

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"monster-db/monster"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type myMonsterServer struct {
+}
+
+func (s myMonsterServer) GetMonster(context.Context, *monster.MonsterName) (*monster.Monster, error) {
+	return &monster.Monster{
+		Name:  &monster.MonsterName{Name: "Orc"},
+		Level: 2,
+	}, nil
+}
 
 func main() {
 	fmt.Println("hellow world")
+
+	lis, err := net.Listen("tcp", ":8081")
+
+	if err != nil {
+		log.Fatalf("cannot create listner: %s", err)
+	}
+
+	serverRegistrar := grpc.NewServer()
+	service := &myMonsterServer{}
+
+	monster.RegisterMonsterServiceServer(serverRegistrar, service)
+
+	err = serverRegistrar.Serve(lis)
+
+	if err != nil {
+		log.Fatalf("cannot serve: %s", err)
+	}
+
 }
